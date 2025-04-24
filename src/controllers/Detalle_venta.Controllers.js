c147823e757153b0ab11275dc88de7efbcb95d80
iDanteh <105389532+iDanteh@users.noreply.github.com>
2025-04-24 09:12:40 -0600

    Base de datos añadida para hacer la importación de la base de datos actualizada en el servidor

--- src/controllers/Detalle_venta.Controllers.js
+++ src/controllers/Detalle_venta.Controllers.js
@@ -2,16 +2,6 @@ import Detalle_Venta from '../models/Detalle_Venta.Model.js';
 import { detalle_VentaRepository } from '../repositories/DetalleVentaRepository.js';
 
 const detalleRepository = new detalle_VentaRepository(Detalle_Venta);
-
-export const getDetalleVenta = async (req, res) => {
-    try {
-        const detalle_venta = await Detalle_Venta.findAll();
-        res.status(200).json(detalle_venta);
-    } catch (error) {
-        res.status(500).json({ error: 'Error al obtener el detalle de las ventas realizadas' });
-    }
-};
-
 export const getVentaById = async (req, res) => {
     try {
         const { venta_id } = req.params;
--- src/routes/DetalleVenta.Routes.js
+++ src/routes/DetalleVenta.Routes.js
@@ -1,8 +1,6 @@
 import { Router } from "express";
-import { getDetalleVenta, getVentaById } from "../controllers/Detalle_venta.Controllers.js";
+import { getVentaById } from "../controllers/Detalle_venta.Controllers.js";
 const router = Router();
-
-router.get('/api/v1/detalleVenta',getDetalleVenta);
 router.get('/api/v1/detalleVenta/:venta_id',getVentaById);
 
 export default router;
