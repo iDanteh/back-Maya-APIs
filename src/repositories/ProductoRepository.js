c5cba874892d85d73636c0a159fb6b888ad8bdad
iDanteh <105389532+iDanteh@users.noreply.github.com>
2025-06-27 09:47:55 -0600

    UPDT: Cambio de base de datos, y actualización para mostrar si un producto cambió de precio

--- src/repositories/ProductoRepository.js
+++ src/repositories/ProductoRepository.js
@@ -8,22 +8,32 @@ export class ProductoRepository {
         return await this.model.findAll();
     }
 
-    async findById(id) {
-        return await this.model.findByPk(id);
+    async findById(codigo_barras) {
+        return await this.model.findByPk(codigo_barras);
     }
 
     async create(productData) {
         return await this.model.create(productData);
     }
 
-    async update(id, productData) {
-        const product = await this.model.findByPk(id);
-        if (!product) return null;
-        return await product.update(productData);
+    async update(codigo_barras, productData) {
+        try {
+            const product = await this.model.findByPk(codigo_barras);
+            if (!product) return null;
+            if (product.precio_maximo !== productData.precio_maximo) {
+                productData.precio_max_anterior = product.precio_maximo;
+                productData.fecha_updt_precio = new Date();
+            }
+
+            return await product.update(productData);
+        } catch (error) {
+            console.log('Error al actualizar el producto:', error);
+            return false;
+        }
     }
 
-    async delete(id) {
-        const product = await this.model.findByPk(id);
+    async delete(codigo_barras) {
+        const product = await this.model.findByPk(codigo_barras);
         if (!product) return false;
         await product.destroy();
         return true;
